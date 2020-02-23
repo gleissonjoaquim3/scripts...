48f3efe60d3f1bd4dec9ddbb8a0a77b64d1a547f
gleissonjoaquim3 <55757359+gleissonjoaquim3@users.noreply.github.com>
2020-02-23 13:54:39 -0300

    Update optimization.sh

--- optimization.sh
+++ optimization.sh
@@ -2,8 +2,9 @@
 
 #SCRIPT QUE FAZ O UPDATE ,VERIFICA SE HÁ ATUALIZAÇÃO E INSTALA OS PROGRAMAS QUE EU MAIS UTILIZO
 
-sudo apt update && sudo apt upgrade && sudo apt install ubuntu-restricted-extras ffmpeg vlc python3-pip python-pip python3-tk python3-virtualenv python3-venv clang wget curl git gparted broadcom-sta-common broadcom-sta-dkms bluez-* pulseaudio-module-bluetooth blueman nautilus-dropbox 
+sudo apt update && sudo apt upgrade && sudo apt install ubuntu-restricted-extras ffmpeg vlc python3-pip python-pip python3-tk python3-virtualenv python3-venv clang wget curl git gparted broadcom-sta-common broadcom-sta-dkms bluez-* pulseaudio-module-bluetooth blueman nautilus-dropbox synapse synaptic musescore3 qelectrotech
 
+#Para instalação ubuntu foi incluido no apt install o synapse synaptic qelectrotech e o musescore3
 
 #Entra no diretório de Download
 cd ~/Downloads
@@ -18,4 +19,4 @@ sudo dpkg -i visual_code.deb
 sudo echo "bcm43xx" | sudo tee -a /etc/modprobe.d/blacklist-broadcom-wireless.conf
 
 #Faz a instalação do fondo via comando flatpak
-flatpak install flathub com.github.calo001.fondo
+#flatpak install flathub com.github.calo001.fondo
